e32e9216250390fe519adb196054c3885c1010c8
Jens Andersen <jensra@gmail.com>
2014-03-07 12:05:17 +0100

    Adding the first version of the Unity project.

---
+++ UnitProject/Assets/Scripts/LightFading.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFading : MonoBehaviour
+{
+	private SpriteRenderer sr;
+
+	public float alphaMin = 0.2f;
+	public float alphaMax = 0.9f;
+
+	public float sinPeriod = 13.0f;
+
+	private float timer;
+
+	// Use this for initialization
+	void Start()
+	{
+		this.sr = this.GetComponent<SpriteRenderer>();
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if( this.sr != null )
+		{
+			this.timer += Time.deltaTime;
+			while( this.timer > sinPeriod )
+				this.timer -= sinPeriod;
+
+			float mid = alphaMin + ( alphaMax / 2.0f );
+			float newAlpha = mid + ( Mathf.Sin( this.timer / sinPeriod ) * ( alphaMax - alphaMin ) );
+
+			this.sr.color = new Color( 1.0f, 1.0f, 1.0f, newAlpha );
+		}
+	}
+}
